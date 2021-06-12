3122ab743aa21b1e4bbfcc0c0195de06aec01e28
Xavi Pavon <xpavon4d@gmail.com>
2021-06-12 15:25:21 +0200

    stickers added + center entrance

---
+++ Assets/scripts/AugmentScale.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AugmentScale : MonoBehaviour
+{
+
+    public float targetScale;
+    public float timeToReachTarget;
+    private float startScale;
+    private float percentScaled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (percentScaled < 1f)
+        {
+            percentScaled += Time.deltaTime / timeToReachTarget; 
+            float scale = Mathf.Lerp(startScale, targetScale, percentScaled);
+            transform.localScale = new Vector3(scale, scale, scale); 
+        }
+    }
+}
---
+++ Assets/scripts/FadeIn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeIn : MonoBehaviour
+{
+    public float timeToReachTarget;
+    private float startOpacity;
+    private float targetOpacity;
+    private float percentFaded;
+    private Color stickerColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startOpacity = 0;
+        targetOpacity = 1;
+        stickerColor = this.GetComponentInChildren<SpriteRenderer>().material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (percentFaded < 1f)
+        {
+            percentFaded += Time.deltaTime / timeToReachTarget;
+            float opacity = Mathf.Lerp(startOpacity, targetOpacity, percentFaded);
+            Color newColor = new Color(stickerColor[0], stickerColor[1], stickerColor[2], opacity);
+            this.GetComponentInChildren<SpriteRenderer>().material.color = newColor;
+        }
+    }
+}
